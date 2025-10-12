df5afa6e24efe4943b2c3facd00b8b2cdc6b6da2
Minami <C5289@oic.jp>
2025-10-12 18:05:30 +0900

    hook dev

--- hook.cpp
+++ hook.cpp
@@ -1,17 +1,26 @@
 #include "hook.h"
 
+Entity2D hook;
+
 // --- 画像読み込み関係 ---
 void hookImage()
 {
 
-
-
+    hook.spriteID = LoadGraph(L"./Data/images/hook_png.png");
+    if (hook.spriteID == -1) {
+        DxPlus::Utils::FatalError(L"failed to load sprite : ./Data/images/hook_png.png");
+    }
 
 }
 
 // --- リセット関数（初期配置や状態の初期化） ---
-void hookReset()
+void hookReset(DxPlus::Vec2 playerBasePosition)
 {
+    hook.position.x = playerBasePosition.x - 100;
+    hook.position.y = playerBasePosition.y - 100;
+
+	hook.scale = { 0.5f,0.5f };
+	hook.center = { 64.0f, 0.0f };
 
 
 }
@@ -25,7 +34,7 @@ void Updatehook(float deltaTime)
 // --- 描画関数 ---
 void hookDraw()
 {
-
+    DxPlus::Sprite::Draw(hook.spriteID, hook.position, hook.scale, hook.center);
 
 }
 
